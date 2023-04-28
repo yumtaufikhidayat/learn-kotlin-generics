878d8730b00c62d2425e353366e9210868415815
yumtaufikhidayat <yumtaufik1997@gmail.com>
2023-04-28 13:43:08 +0700

    Implement generic function.

---
+++ src/main/kotlin/generic/GenericFunction.kt
@@ -0,0 +1,20 @@
+package main.kotlin.generic
+
+fun main() {
+    genericFunctions()
+}
+
+fun genericFunctions() {
+    val function = Function("Taufik")
+    function.sayHello<String>("Zharfan")
+    function.sayHello("Wafiq")
+
+    function.sayHello<Int>(10)
+    function.sayHello(10)
+}
+
+class Function(val name: String) {
+    fun <T> sayHello(param: T) {
+        println("Hello $param, my name is $name")
+    }
+}
