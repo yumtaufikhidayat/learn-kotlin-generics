8a98233ae9763721156c9a8c82a44211689e06b9
yumtaufikhidayat <yumtaufik1997@gmail.com>
2023-05-06 09:42:25 +0700

    Implement generic constraint using where.

--- src/main/kotlin/generic/GenericConstraint.kt
+++ src/main/kotlin/generic/GenericConstraint.kt
@@ -1,14 +1,23 @@
 package main.kotlin.generic
 
 fun main() {
-    val data1 = Company(Manager())
+    // val data1 = Company(Manager()) // error mismatch. Found Manager required CanSayHello
     val data2 = Company(VicePresident())
+    //val data3 = Company("String") // error mismatch. Found String required CanSayHello and Employee
 }
 
 open class Employee
 
 class Manager: Employee()
 
-class VicePresident: Employee()
+class VicePresident: Employee(), CanSayHello {
+    override fun sayHello(name: String) {
+        println("Hello $name")
+    }
+}
 
-class Company<T: Employee>(val employee: T)
+class Company<T>(val employee: T) where T: Employee, T: CanSayHello
+
+interface CanSayHello {
+    fun sayHello(name: String)
+}
