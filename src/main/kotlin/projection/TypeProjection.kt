0cb687b0ae2b765e1cf2f178a53102e713dfc50c
yumtaufikhidayat <yumtaufik1997@gmail.com>
2023-05-07 07:53:09 +0700

    Add println to type projection function.

--- src/main/kotlin/projection/TypeProjection.kt
+++ src/main/kotlin/projection/TypeProjection.kt
@@ -7,7 +7,7 @@ fun main() {
 fun typeProjection() {
     val data1 = Container("Data 1")
     val data2: Container<Any> = Container("Data 2")
-    copy(data1, data2)
+    println(copy(data1, data2))
 }
 
 fun copy(from: Container<out Any>, to: Container<Any>) {
