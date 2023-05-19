9b88a18fe21a3335a8cece71ffa183e4fd886b90
yumtaufikhidayat <yumtaufik1997@gmail.com>
2023-05-20 06:14:46 +0700

    Implement comparable interface.

---
+++ src/main/kotlin/interface/Comparable.kt
@@ -0,0 +1,16 @@
+package main.kotlin.`interface`
+
+fun main() {
+    comparable()
+}
+
+fun comparable() {
+    val fruit1 = Fruit("Mangga", 10)
+    val fruit2 = Fruit("Apel", 100)
+    println(fruit1 > fruit2)
+    println(fruit1 < fruit2)
+}
+
+class Fruit(val name: String, val quantity: Int): Comparable<Fruit> {
+    override fun compareTo(other: Fruit): Int = quantity.compareTo(other.quantity)
+}
