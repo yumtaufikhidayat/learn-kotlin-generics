a0ababac1dcf58fcd6d5599574b09e3f13a83019
yumtaufikhidayat <yumtaufik1997@gmail.com>
2023-05-27 14:42:48 +0700

    Implement observable property.

---
+++ src/main/kotlin/interface/ObservableProperty.kt
@@ -0,0 +1,29 @@
+package main.kotlin.`interface`
+
+import kotlin.properties.ObservableProperty
+import kotlin.reflect.KProperty
+
+fun main() {
+    observableProperty()
+}
+
+fun observableProperty() {
+    val car = Car("Audi")
+    car.brand = "Volkswagen"
+    println(car.brand)
+}
+
+class Car(brand: String) {
+    var brand: String by LogObservableProperty(brand)
+}
+
+class LogObservableProperty<T>(data: T): ObservableProperty<T>(data) {
+    override fun beforeChange(property: KProperty<*>, oldValue: T, newValue: T): Boolean {
+        println("Before change value from $oldValue to $newValue")
+        return true
+    }
+
+    override fun afterChange(property: KProperty<*>, oldValue: T, newValue: T) {
+        println("After change value from $oldValue to $newValue")
+    }
+}
